4ff63738036506a85093cb37cabb045b96d81126
olalekan <hammed@getrova.com>
2023-10-30 13:17:00 +0100

    initital setup

---
+++ src/main/java/com/prophius/socialmediaservice/SocialmediaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.prophius.socialmediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prophius/socialmediaservice/SocialmediaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prophius.socialmediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
