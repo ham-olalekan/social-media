b5c6cfab3d407eb46676db78ad89455e6a90b70a
olalekan <hammed@getrova.com>
2023-10-31 00:08:02 +0100

    setup user authorities enum

---
+++ src/main/java/com/prophius/socialmediaservice/enums/Authorities.java
@@ -0,0 +1,18 @@
+package com.prophius.socialmediaservice.enums;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public enum Authorities {
+    USER;
+
+    public static final String USER_PREAUTHORIZE = "hasAuthority('USER')";
+
+    public static Authorities[] DEFAULT(Authorities... authorities) {
+        HashSet<Authorities> authoritiesHashSet = new HashSet<>();
+        authoritiesHashSet.add(USER);
+        authoritiesHashSet.addAll(Arrays.stream(authorities).collect(Collectors.toSet()));
+        return authoritiesHashSet.toArray(new Authorities[]{});
+    }
+}
